c820556b6a0207802b14078762fe31d5b17a1e3c
dikobrazik <ilnar.khusaenov@gmail.com>
2024-02-06 16:19:12 +0300

    jwt added to group module

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { GroupsController } from './groups.controller';
-import { GroupsService } from './groups.service';
+import {Module} from '@nestjs/common';
+import {GroupsController} from './groups.controller';
+import {GroupsService} from './groups.service';
+import {JwtModule} from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule],
   controllers: [GroupsController],
-  providers: [GroupsService]
+  providers: [GroupsService],
 })
 export class GroupsModule {}
