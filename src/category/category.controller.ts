ec6bab82df1405c848cb874eb8a855e98a727dd3
dikobrazik <ilnar.khusaenov@gmail.com>
2025-01-24 11:12:05 +0300

    api for categories icons

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,11 +1,30 @@
-import {Controller, Get, NotFoundException, Param, Query} from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  NotFoundException,
+  Param,
+  Query,
+  StreamableFile,
+} from '@nestjs/common';
 import {CategoryService} from './category.service';
 import {ApiTags} from '@nestjs/swagger';
 import {FindOneParams} from 'src/dtos/category';
+import {createReadStream, existsSync} from 'fs';
+import {join} from 'path';
+
+const MONTH_IN_SECONDS = 60 * 60 * 24 * 30;
 
 @ApiTags('Categories')
 @Controller('categories')
 export class CategoryController {
+  private defaultIconPath = join(
+    process.cwd(),
+    'public',
+    'categories-icons',
+    'default.svg',
+  );
+
   constructor(private categoryService: CategoryService) {}
 
   @Get()
@@ -30,6 +49,26 @@ export class CategoryController {
     }
 
     return category;
+  }
+
+  @Get(':id/icon')
+  @Header('Content-Type', 'image/svg+xml')
+  @Header('Cache-Control', `max-age=${MONTH_IN_SECONDS}, public`)
+  getFileUsingStaticValues(@Param() params: FindOneParams): StreamableFile {
+    const iconPath = join(
+      process.cwd(),
+      'public',
+      'categories-icons',
+      `${params.id}.svg`,
+    );
+
+    if (existsSync(iconPath)) {
+      const icon = createReadStream(iconPath);
+      return new StreamableFile(icon);
+    }
+
+    const defaultIcon = createReadStream(this.defaultIconPath);
+    return new StreamableFile(defaultIcon);
   }
 
   @Get(':id/is-popular')
